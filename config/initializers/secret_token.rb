6b49b7161cd64c9e76f28c50017373afa7f4ae3a
redi <reidy.sianto@ukirama.com>
2013-10-22 10:08:37 +0700

    first template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megacorp::Application.config.secret_token = '316cb18ab73b3840660bbf1da5947fecb31bf63d97b68477446bce9d1beb8cccad11b03ff43f63c816b8428b09016e73492db034f1d52cba673b9f2ee295c95b'
